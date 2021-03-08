697a76be1211aec493298b0471cc1d2c773ad2b7
Katie <kmurley@gmail.com>
2021-03-08 21:33:29 +0100

    .

--- calibration_end_to_end.py
+++ calibration_end_to_end.py
@@ -10,18 +10,20 @@ from datetime import date
 import math
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import cal_routines as cal
 
-
+base_dir='/vol/astro7/lofar/kmulrey/calibration'
 
 
 
 parser = OptionParser()
 
-parser.add_option("-a", "--antenna", default = "1", help = "antenna ID")
+parser.add_option('-f', '--model_folder', default = 'jones_standard', help = 'jones matrix folder')
 (options, args) = parser.parse_args()
 
-AART_ant_id = int(options.antenna)
+antenna_model_folder = int(options.antenna)
 
+jones_dir=base_dir+antenna_model_folder
 
-
+print(jones_dir)
 #data_dir='/vol/astro3/lofar/sim/kmulrey/calibration/final'
