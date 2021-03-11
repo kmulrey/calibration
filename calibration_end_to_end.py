60566cf862ccecf518391bad5002085dfff697ed
Katie <kmurley@gmail.com>
2021-03-11 21:21:24 +0100

    .

--- calibration_end_to_end.py
+++ calibration_end_to_end.py
@@ -115,7 +115,7 @@ for f in np.arange(51):
         consol_flag=1
         print('no consolidated info {0}'.format(consolidate_dir+'/consolidated_'+name))
    
-if consol_flag=1:
+if consol_flag==1:
     print('now consolidating info')
     if not os.path.exists(consolidate_dir):
         os.makedirs(consolidate_dir)
