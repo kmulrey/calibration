c58ab6fbb7869fd5c4f7b5f212d7f82fe501be08
Katie <kmurley@gmail.com>
2021-03-24 08:59:15 +0100

    .

--- cal_routines.py
+++ cal_routines.py
@@ -578,8 +578,8 @@ def do_fit(consolidate_dir,fit_data_dir,fit_dir,name,station,ant_id):
     cable_attenuation_80=np.genfromtxt(fit_data_dir+'/attenuation/attenuation_coax9_80m.txt',usecols=1)
     cable_attenuation_115=np.genfromtxt(fit_data_dir+'/attenuation/attenuation_coax9_115m.txt',usecols=1)
     RCU_gain=np.genfromtxt(fit_data_dir+'/RCU_gain_new_5.txt',usecols=0)
-    #jones_vals=np.genfromtxt(fit_data_dir+'/antenna_gain.txt')
-    jones_vals=121*np.ones([51])
+    jones_vals=np.genfromtxt(fit_data_dir+'/antenna_gain.txt')
+    #jones_vals=121*np.ones([51])
 
 
     g=11.08423462
