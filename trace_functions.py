0d86d971aaf235830007d428012697d2bb2799ad
Katie <kmurley@gmail.com>
2021-04-30 15:25:00 +0200

    .

--- trace_functions.py
+++ trace_functions.py
@@ -70,3 +70,35 @@ def return_cal(caltype):
     Calibration_curve[29:82]=np.array([0, 1.37321451961e-05,1.39846332239e-05,1.48748993821e-05,1.54402170354e-05,1.60684568225e-05,1.66241942741e-05,1.67039066047e-05,1.74480931848e-05,1.80525736486e-05,1.87066855054e-05,1.88519099831e-05,1.99625051386e-05,2.01878566584e-05,2.11573680797e-05,2.15829455528e-05,2.20133824866e-05,2.23736319125e-05,2.24484419697e-05,2.37802483891e-05,2.40581543111e-05,2.42020383477e-05,2.45305869187e-05,2.49399905965e-05,2.63774023804e-05,2.70334253414e-05,2.78034857678e-05,3.07147991391e-05,3.40755705892e-05,3.67311849851e-05,3.89987440028e-05,3.72257913465e-05,3.54293510934e-05,3.35552370942e-05,2.96529815929e-05,2.79271252352e-05,2.8818544973e-05,2.92478843809e-05,2.98454768706e-05,3.07045462103e-05,3.07210553534e-05,3.16442871206e-05,3.2304638838e-05,3.33203882046e-05,3.46651060935e-05,3.55193137077e-05,3.73919275937e-05,3.97397037914e-05,4.30625048727e-05,4.74612081994e-05,5.02345866124e-05, 5.53621848304e-05,0])
 
     return Calibration_curve,Calibration_curve_new
+
+
+def get_data(event, station, Calibration_curve, Calibration_curve_new):
+    file=open('/vol/astro3/lofar/vhecr/lora_triggered/results_with_abs_calibration/'+event+'/calibrated_pulse_block-'+event+'-'+station+'.npy','rb')
+    info=np.load(file,encoding='bytes')
+    file.close()
+    nAntData=len(info[::2])
+    print('{0} antennas'.format(nAntData))
+    
+    dt=5.0e-9
+    time0=np.arange(0,len(info[0]))*dt
+    freq=np.fft.rfftfreq(len(info[0]),dt)
+    
+    data_0pol=info[::2]
+    data_1pol=info[1::2]
+
+    data_0pol_fft=np.fft.rfft(data_0pol)
+    data_1pol_fft=np.fft.rfft(data_1pol)
+    
+    Calibration_curve_interp = interp1d(np.linspace(0.e6,100e6,101), Calibration_curve, kind='linear')
+    Calibration_curve_interp = Calibration_curve_interp(freq)
+
+    Calibration_curve_new_interp = interp1d(np.linspace(0.e6,100e6,101), Calibration_curve_new, kind='linear')
+    Calibration_curve_new_interp = Calibration_curve_new_interp(freq)
+    
+    data_0pol_fft_new=np.nan_to_num((Calibration_curve_new_interp/Calibration_curve_interp))*data_0pol_fft
+    data_1pol_fft_new=np.nan_to_num((Calibration_curve_new_interp/Calibration_curve_interp))*data_1pol_fft
+    
+    data_0pol_new=np.fft.irfft(data_0pol_fft_new)
+    data_1pol_new=np.fft.irfft(data_1pol_fft_new)
+    
+    return time0,data_0pol_new,data_1pol_new
